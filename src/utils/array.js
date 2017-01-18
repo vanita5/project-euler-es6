21a860dbe1d18a91841461008b41c8ef03125f35
vanita5 <mail@vanit.as>
2017-01-18 12:11:47 +0100

    Problem #29

--- src/utils/array.js
+++ src/utils/array.js
@@ -15,6 +15,24 @@ class ArrayUtils {
     static create2DArray(width, height, def = undefined) {
         return Array(height).fill().map(() => Array(width).fill(def));
     }
+
+    /**
+     * Check if array contains bignum.
+     * Array.prototype.includes() fails.
+     *
+     * Compare string representation as it is faster.
+     *
+     * @param arr
+     * @param bignum
+     * @return {boolean}
+     */
+    static bignumArrayContains(arr, bignum) {
+        const compareString = bignum.toString();
+        for (const b of arr) {
+            if (compareString === b.toString()) return true;
+        }
+        return false;
+    }
 }
 
 export default ArrayUtils;
